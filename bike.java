1acd6b646c092546c0e1a9aa92c596145887dd43
babugowtham36 <babugowtham36@gmail.com>
2025-12-03 08:31:43 +0530

    Add files via upload

---
+++ bike.java
@@ -0,0 +1,12 @@
+class bike
+{
+	static String bike_name="hero";
+	static String bike_color="black";
+	static double bike_cost=50000d;
+	public static void main(String [] args)
+	{
+		System.out.println(" the bike name is "+bike_name);
+		System.out.println(" the bike color is "+bike_color);
+		System.out.println(" the bike cost is "+bike_cost);
+	}
+}
